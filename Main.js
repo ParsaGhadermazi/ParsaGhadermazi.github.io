6d043d813603c326e8e5617164ba85a1c9508e7b
ParsaGhadermazi <54489047+ParsaGhadermazi@users.noreply.github.com>
2022-03-11 00:54:16 -0700

    some interactvity

---
+++ Main.js
@@ -0,0 +1,15 @@
+function pubMsg() {
+    alert("Not much yet, but stay tuned!");
+}
+
+function getDate() {
+    document.getElementById("date").innerHTML = Date();
+
+}
+
+
+function Switch() {
+    var x = document.getElementById("switch_ON");
+    x.className = "switch_OFF";
+
+}
